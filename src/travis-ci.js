85251cde918a0e383bdbdb2c8af5a2447bbb2615
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2013-09-12 22:14:30 +0200

    Fixed travis.ci branch variable bug, added empty test

--- src/travis-ci.js
+++ src/travis-ci.js
@@ -2,6 +2,6 @@
  * Return Travis-ci Markdown badge as string.
  */
 exports.badgeMarkdown = function(githubUsername, githubProject, branch) {
-	badge = badge || 'master';
+	branch = branch || 'master';
 	return '[![Build Status](https://travis-ci.org/'+githubUsername+'/'+githubProject+'.png?branch='+branch+')](https://travis-ci.org/'+githubUsername+'/'+githubProject+')\n\n';
 }
---
+++ test/test.travis-ci.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+var travis = require('./../src/travis-ci');
+
+
+describe('src/travis-ci.js', function() {
+  
+  
+
+})
