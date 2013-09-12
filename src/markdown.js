5b681347f27906c65d21ea0199a30678292db341
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2013-09-12 22:09:37 +0200

    Added markdown.js and test cases

---
+++ src/markdown.js
@@ -0,0 +1,69 @@
+/**
+ * Some Markdown functions to generate links, lists etc. strings.
+ */
+
+exports.link = function(url, title) {
+  return '['+title+']('+url+')';
+}
+
+exports.image = function(url, altText) {
+  altText = altText || '';
+  return '!['+altText+']('+url+')';
+}
+
+exports.header1 = function(text) {
+  return '# '+text;
+}
+
+exports.header2 = function(text) {
+  return '## '+text;
+}
+
+exports.header3 = function(text) {
+  return '### '+text;
+}
+
+exports.header4 = function(text) {
+  return '#### '+text;
+}
+
+exports.header5 = function(text) {
+  return '##### '+text;
+}
+
+exports.header6 = function(text) {
+  return '###### '+text;
+}
+
+exports.blockquotes = function(text) {
+  return '> '+text;
+}
+
+exports.inlineCode = function(code) {
+  return '`'+code+'`';
+}
+
+exports.blockCode = function(code) {
+  return '    '+code;
+}
+
+
+exports.rule = function() {
+  return '---';
+}
+
+exports.list = function(arr) {
+  var tmp = '';
+  for (var i=0; i<arr.length; i++) {
+    tmp += '- '+arr[i]+'\n';
+  };
+  return tmp;
+}
+
+exports.listOrdered = function(arr) {
+  var tmp = '';
+  for (var i=0; i<arr.length; i++) {
+    tmp += (i+1)+'. '+arr[i]+'\n';
+  };
+  return tmp;
+}
---
+++ test/test.markdown.js
@@ -0,0 +1,91 @@
+var assert = require('assert');
+var markdown = require('./../src/markdown');
+
+
+describe('src/markdown.js', function() {
+
+  describe('#link()', function() {
+    it('should return a link.', function() {
+      assert.equal( '[baz](http://www.foo.bar/)', markdown.link('http://www.foo.bar/', 'baz') );
+    })
+  })
+
+  describe('#image()', function() {
+    it('should return a image.', function() {
+      assert.equal( '![baz](http://www.foo.bar/)', markdown.image('http://www.foo.bar/', 'baz') );
+    })
+  })
+
+  describe('#header1()', function() {
+    it('should return a header1.', function() {
+      assert.equal( '# foo', markdown.header1('foo') );
+    })
+  })
+
+  describe('#header2()', function() {
+    it('should return a header2.', function() {
+      assert.equal( '## foo', markdown.header2('foo') );
+    })
+  })
+
+  describe('#header3()', function() {
+    it('should return a header3.', function() {
+      assert.equal( '### foo', markdown.header3('foo') );
+    })
+  })
+
+  describe('#header4()', function() {
+    it('should return a header4.', function() {
+      assert.equal( '#### foo', markdown.header4('foo') );
+    })
+  })
+
+  describe('#header5()', function() {
+    it('should return a header5.', function() {
+      assert.equal( '##### foo', markdown.header5('foo') );
+    })
+  })
+
+  describe('#header6()', function() {
+    it('should return a header6.', function() {
+      assert.equal( '###### foo', markdown.header6('foo') );
+    })
+  })
+
+  describe('#blockquotes()', function() {
+    it('should return a blockquotes.', function() {
+      assert.equal( '> foo', markdown.blockquotes('foo') );
+    })
+  })
+
+  describe('#inlineCode()', function() {
+    it('should return a inlineCode.', function() {
+      assert.equal( '`foo`', markdown.inlineCode('foo') );
+    })
+  })
+
+  describe('#blockCode()', function() {
+    it('should return a blockCode.', function() {
+      assert.equal( '    foo', markdown.blockCode('foo') );
+    })
+  })
+
+  describe('#rule()', function() {
+    it('should return a rule.', function() {
+      assert.equal( '---', markdown.rule() );
+    })
+  })
+
+  describe('#list()', function() {
+    it('should return a list.', function() {
+      assert.equal( '- foo\n- bar\n- baz\n', markdown.list(['foo', 'bar', 'baz']) );
+    })
+  })
+
+  describe('#listOrdered()', function() {
+    it('should return a listOrdered.', function() {
+      assert.equal( '1. foo\n2. bar\n3. baz\n', markdown.listOrdered(['foo', 'bar', 'baz']) );
+    })
+  })
+
+})
