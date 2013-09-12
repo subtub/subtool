2251506c65ac1d4d46ae9583012054340a72de4b
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2013-09-12 10:18:19 +0200

    WIP at readme.js

--- src/readme.js
+++ src/readme.js
@@ -3,36 +3,21 @@
  * Everything is formatted in Markdown
  */
 
-var fs = require('fs');
-var path = require('path');
-var license = require('./license.js');
 
+/**
+ * Module dependencies.
+ */
+var utils = require('./utils');
+var license = require('./license');
 
 /**
  * The Readme headline.
  */
-function headline(callback) {
-  // The package.json filepath
-  var pkgPath = process.env.PWD+'/package.json';
-  // check if a package.json exist
-  fs.exists(pkgPath, function(data) {
-    if (data === true) {
-      // read the file
-      fs.readFile(pkgPath, function(err, data) {
-        var pkg = JSON.parse(data);
-        // check if the name and version value exist
-        var name = null;
-        if (pkg.name) name = pkg.name;
-        var version = null;
-        if (pkg.version) version = pkg.version;
-        return callback('# '+name+' v'+version+'  \n');
-      });
-    }
-    // if no package.json was found, we use the directory name.
-    else {
-      return callback('# '+path.basename(process.env.PWD)+'  \n');
-    };
-  });
+function headline(name, version) {
+  var tmp = '# ';
+  if (name) tmp += name;
+  if (version) tmp += ' v'+version;
+  return tmp+'  \n';
 }
 exports.headline = headline;
 
@@ -40,9 +25,8 @@ exports.headline = headline;
  * Generated at information.
  */
 function generatedInfo() {
-  var content = '---\n\n'+
-                '*This Readme was generated on '+Date()+'.*  \n';
-  return content;
+  return '\n---\n\n'+
+         '*This Readme was generated on '+Date()+'.*  \n';
 }
 
 /**
@@ -50,13 +34,15 @@ function generatedInfo() {
  */
 function readme(callback) {
   if (callback && typeof(callback) === "function") {
-    var tmp = null;
-    headline(function(data) {
-      tmp = data;
-      tmp = '## License';
-      tmp += license.mit();
-      tmp += generatedInfo();
-      return callback(tmp);
+    utils.readPackageJson(process.env.PWD+'/package.json', function(data) {
+      var tmp = headline(data.name, data.version)+'\n';
+
+      license.mit('subtub', '2013', function(data) {
+        tmp += '## License\n\n'+data;
+
+        tmp += generatedInfo();
+        return callback(tmp);
+      });
     });
   }
 }
--- test/test.readme.js
+++ test/test.readme.js
@@ -1,20 +1,26 @@
 var assert = require('assert');
 var readme = require('./../src/readme');
 
+
 describe('src/readme.js', function() {
+  
   describe('#headline()', function() {
-    it('should return the readme headline.', function() {
-      readme.headline(function(data) {
-        assert.equal( '# subtool v0.0.1  \n', data );
-      })
+    it('should return the readme headline without param.', function() {
+      assert.equal( '#   \n', readme.headline() );
+    })
+    it('should return the readme headline with param name.', function() {
+      assert.equal( '# foo  \n', readme.headline('foo') );
+    })
+    it('should return the readme headline with param name, version.', function() {
+      assert.equal( '# foo v0.0.0  \n', readme.headline('foo', '0.0.0') );
     })
   })
 
-  describe('#readme()', function() {
-    it('should return the readme headline.', function() {
-      readme.readme(function(data) {
-        assert.equal( 'foo', data );
-      })
-    })
-  })
+  // describe('#readme()', function() {
+  //   it('should return the readme headline.', function() {
+  //     readme.readme(function(data) {
+  //       assert.equal( 'foo', data );
+  //     })
+  //   })
+  // })
 })
