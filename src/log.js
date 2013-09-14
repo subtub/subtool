7cff4f8fa76555583b6f6970fd47b244ffea3e8e
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2013-09-14 11:16:48 +0200

    log.js added color support

--- src/log.js
+++ src/log.js
@@ -1,10 +1,38 @@
 /**
- * Generated at information.
+ * Module dependencies.
  */
-module.exports = function(message, silent) {
-  silent = silent || false;
+var clc = require('cli-color');
+
+/**
+ * Generated at information.
+ *
+ * @param config - object like: {silent: true, color: 'green'}
+ */
+module.exports = function(message, config) {
+  var silent = false;
+  var color = false;
+
+  // check the config values
+  if (config !== undefined) {
+  	if (config.silent !== undefined) silent = config.silent;
+  	if (config.color !== undefined) {
+  		if (config.color === 'red') color = clc.red;
+  		else if (config.color === 'green') color = clc.green;
+  		else if (config.color === 'yellow') color = clc.yellow;
+  		else if (config.color === 'blue') color = clc.blue;
+  		else if (config.color === 'magenta') color = clc.magenta;
+  		else if (config.color === 'cyan') color = clc.cyan;
+  		else if (config.color === 'white') color = clc.white;
+  	}
+  }
+
+  // log the message
   if (silent === false) {
-    console.log(message);
+  	if (color === false) {
+      console.log(message);
+    } else {
+    	console.log(color(message));
+    }
     return message;
   } else {
   	return '';
--- test/test.log.js
+++ test/test.log.js
@@ -8,8 +8,19 @@ describe('src/log.js', function() {
     it('log a message to the console.', function() {
       assert.equal( 'foo', log('foo') );
     })
+
+    it('log a message to the console, with red color.', function() {
+      assert.equal( 'foo', log('foo', {color: 'red'}) );
+    })
+    it('log a message to the console, with green color.', function() {
+      assert.equal( 'foo', log('foo', {color: 'green'}) );
+    })
+    it('log a message to the console, with blue color.', function() {
+      assert.equal( 'foo', log('foo', {color: 'blue'}) );
+    })
+    
     it('log nothing because silent mode is active.', function() {
-      assert.equal( '', log('foo', true) );
+      assert.equal( '', log('foo', {silent: true}) );
     })
   })
 
