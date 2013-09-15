e868d2f3635627f73f87e723c3f09d8d35715517
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2013-09-15 11:48:56 +0200

    travis-ci.js added save Yml function, added tests

--- src/travis-ci.js
+++ src/travis-ci.js
@@ -1,14 +1,37 @@
 /**
  * Module dependencies.
  */
+var fs = require('fs');
 var md = require('./markdown');
+
+var FILENAME = '.travis.yml';
 
 /**
  * Return Travis-ci Markdown badge as string.
  */
 exports.badgeMarkdown = function(githubUsername, githubProject, branch) {
-	branch = branch || 'master';
-	var img = 'https://travis-ci.org/'+githubUsername+'/'+githubProject+'.png?branch='+branch;
-	var url = 'https://travis-ci.org/'+githubUsername+'/'+githubProject;
-	return md.link(url, md.image(img, 'Build Status'));
+  branch = branch || 'master';
+  var img = 'https://travis-ci.org/'+githubUsername+'/'+githubProject+'.png?branch='+branch;
+  var url = 'https://travis-ci.org/'+githubUsername+'/'+githubProject;
+  return md.link(url, md.image(img, 'Build Status'));
+}
+
+/**
+ * Create .travis.yml file
+ *
+ * @param config - like {language: 'node_js', version: '0.8'}
+ */
+exports.saveYml = function(path, config, callback) {
+  var tmp = 'language: '+config.language+'\n';
+  tmp += config.language+':\n';
+  tmp += '- '+config.version;
+
+  fs.writeFile(path+'/'+FILENAME, tmp, function (err) {
+    if(err) {
+      //console.log(err);
+      return callback(false);
+    } else {
+      return callback(true);
+    }
+  });
 }
--- test/test.travis-ci.js
+++ test/test.travis-ci.js
@@ -4,6 +4,17 @@ var travis = require('./../src/travis-ci');
 
 describe('src/travis-ci.js', function() {
   
-  
+  describe('#saveYml()', function() {
+  	it('should return true, if the file was saved', function() {
+  		travis.saveYml(process.env.PWD+'/testing', {language: 'node_js', version: '0.8'}, function(data) {
+  			assert.equal( true, data );
+  		})
+    })
+    it('should return false, if an error occurred', function() {
+  		travis.saveYml(process.env.PWD+'/not/correct', {language: 'node_js', version: '0.8'}, function(data) {
+  			assert.equal( false, data );
+  		})
+    })
+  })
 
 })
