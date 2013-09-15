cd3adde1f22f3f740f9d43d1ebd2f1e1857ba56f
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2013-09-15 14:50:46 +0200

    test.travis cleanup

--- test/test.travis-ci.js
+++ test/test.travis-ci.js
@@ -6,20 +6,17 @@ describe('src/travis-ci.js', function() {
   
   var config = {language: 'node_js', version: ['0.8', '0.10']};
 
-  describe('#saveYml()', function() {
-  	
+  describe('#saveYml() with callback', function() {
     it('should return true, if the file was saved', function() {
-  		travis.saveYml(process.env.PWD+'/testing', config, function(data) {
-  			assert.equal( true, data );
-  		})
+      travis.saveYml(process.env.PWD+'/testing', config, function(data) {
+        assert.equal( true, data );
+      })
     })
-
     it('should return false, if an error occurred', function() {
-  		travis.saveYml(process.env.PWD+'/not/correct', config, function(data) {
-  			assert.equal( false, data );
-  		})
+      travis.saveYml(process.env.PWD+'/not/correct', config, function(data) {
+        assert.equal( false, data );
+      })
     })
-
   })
 
 })
