bd48963aec939f0ad62ee8ec62ff6c4e4a89ce1b
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2013-09-15 19:20:15 +0200

    travis-ci.js check params at .badgeMarkdown(), generateYml(), update saveYml(), added tests

--- src/travis-ci.js
+++ src/travis-ci.js
@@ -10,22 +10,46 @@ var FILENAME = '.travis.yml';
  * Return Travis-ci Markdown badge as string.
  */
 exports.badgeMarkdown = function(githubUsername, githubProject, branch) {
-  branch = branch || 'master';
-  var img = 'https://travis-ci.org/'+githubUsername+'/'+githubProject+'.png?branch='+branch;
-  var url = 'https://travis-ci.org/'+githubUsername+'/'+githubProject;
-  return md.link(url, md.image(img, 'Build Status'));
+  if (githubUsername !== undefined && githubProject !== undefined) {
+    branch = branch || 'master';
+    var img = 'https://travis-ci.org/'+githubUsername+'/'+githubProject+'.png?branch='+branch;
+    var url = 'https://travis-ci.org/'+githubUsername+'/'+githubProject;
+    return md.link(url, md.image(img, 'Build Status'));
+  } else {
+    return false;
+  };
 }
 
 /**
  * Generate the content for a travis file.
+ *
+ * @param config - object like {language: 'node_js', version: ['0.8', '0.10']}
  */
-exports.generateYml = function(config) {
-  var tmp = 'language: node_js\n';
-  tmp += 'node_js:\n';
-  tmp += '  - 0.8\n';
-  tmp += '  - 0.10';
-  return tmp;
+function generateYml(config) {
+  // If config object is not valid
+  if (config === undefined ||
+      config.language === undefined ||
+      config.version === undefined) {
+    return false;
+  }
+  // If it is valid
+  else {
+    var tmp = 'language: '+config.language+'\n';
+    tmp += config.language+':\n';
+    // If config.version is array
+    if (config.version instanceof Array) {
+      for (var i=0; i<config.version.length; i++) {
+        tmp += '  - '+config.version[i]+'\n';  
+      };
+    }
+    // if it is a string
+    else if (typeof config.version === 'string'){
+      tmp += '  - '+config.version+'\n';
+    };
+    return tmp;
+  };
 }
+exports.generateYml = generateYml;
 
 /**
  * Create .travis.yml file
@@ -33,14 +57,7 @@ exports.generateYml = function(config) {
  * @param config - like {language: 'node_js', version: '0.8'}
  */
 exports.saveYml = function(path, config, callback) {
-  var tmp = 'language: '+config.language+'\n';
-  tmp += config.language+':\n';
-
-  for (var i=0; i<config.version.length; i++) {
-    tmp += '  - '+config.version[i]+'\n';
-  };
-
-  fs.writeFile(path+'/'+FILENAME, tmp, function (err) {
+  fs.writeFile(path+'/'+FILENAME, generateYml(config), function (err) {
     if(err) {
       //console.log(err);
       return callback(false);
--- test/test.travis-ci.js
+++ test/test.travis-ci.js
@@ -4,16 +4,44 @@ var travis = require('./../src/travis-ci');
 
 describe('src/travis-ci.js', function() {
   
-  var config = {language: 'node_js', version: ['0.8', '0.10']};
-  
-  describe('#saveYml() with callback', function() {
-    it('should return true, if the file was saved', function() {
-      var config = {language: 'node_js', version: ['0.8', '0.10']}
-      assert.equal( 'language: node_js\nnode_js:\n  - 0.8\n  - 0.10', travis.generateYml(config) );
+  describe('#badgeMarkdown()', function() {
+    it('should return a string if all parameters are valid', function() {
+      assert.equal( '[![Build Status](https://travis-ci.org/subtub/subtool.png?branch=develop)](https://travis-ci.org/subtub/subtool)',
+                    travis.badgeMarkdown('subtub', 'subtool', 'develop') );
+    })
+    it('should return a string if the user and project parameters are valid', function() {
+      assert.equal( '[![Build Status](https://travis-ci.org/subtub/subtool.png?branch=master)](https://travis-ci.org/subtub/subtool)',
+                    travis.badgeMarkdown('subtub', 'subtool') );
+    })
+    it('should return false if a parameter is not valid', function() {
+      assert.equal( false, travis.badgeMarkdown('subtub') );
+    })
+    it('should return false if a parameter is not valid', function() {
+      assert.equal( false, travis.badgeMarkdown() );
     })
   })
 
-  describe('#saveYml() with callback', function() {
+  describe('#generateYml()', function() {
+    var config = {language: 'node_js', version: ['0.8', '0.10']};
+
+    it('should return the generated .travis.yml content as string', function() {
+      var config = {language: 'node_js', version: ['0.8', '0.10']};
+      assert.equal( 'language: node_js\nnode_js:\n  - 0.8\n  - 0.10\n', travis.generateYml(config) );
+    })
+    it('should return the generated .travis.yml content as string', function() {
+      var config = {language: 'node_js', version: '0.10'};
+      assert.equal( 'language: node_js\nnode_js:\n  - 0.10\n', travis.generateYml(config) );
+    })
+    it('should return false is the config object is not correct', function() {
+      var config = {language: 'node_js'};
+      assert.equal( false, travis.generateYml(config) );
+    })
+    it('should return false if no config object exist', function() {
+      assert.equal( false, travis.generateYml() );
+    })
+  })
+
+  describe('#saveYml()', function() {
     it('should return true, if the file was saved', function() {
       travis.saveYml(process.env.PWD+'/testing', config, function(data) {
         assert.equal( true, data );
